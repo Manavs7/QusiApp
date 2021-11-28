abb25f1beef4aed424913ec74e2dec4e0ad14a0f
Manavs7 <singhmann395@gmail.com>
2021-11-28 21:40:57 +0100

    Gip project: Qusi

--- app/src/main/java/com/example/qusi/MainActivity.kt
+++ app/src/main/java/com/example/qusi/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.qusi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Button click
+        goToLogin()
     }
+
+
+    //function for Button Click-> new activity
+    private fun goToLogin(){
+        val button:Button = findViewById(R.id.homebutton)
+        button.setOnClickListener {
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
+
