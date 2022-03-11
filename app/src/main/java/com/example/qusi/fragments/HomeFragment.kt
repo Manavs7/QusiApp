c3537d1e43b6001947233abb7f05a213a11c94f0
Manavs7 <95098034+Manavs7@users.noreply.github.com>
2022-03-11 11:19:39 +0100

    macro

--- app/src/main/java/com/example/qusi/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/qusi/fragments/HomeFragment.kt
@@ -16,6 +16,34 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var v: View = inflater.inflate(R.layout.fragment_home, container, false)
+        
+        /* 
+         //Database oproepen
+         val helper = SQLiteHelper(requireContext())
+        val db = helper.readableDatabase
+        val rs = db.rawQuery("SELECT * FROM USERS",null)
+        //Varaibeles data user
+         val username = rs.getString(1).toDouble()
+        val age = rs.getString(2).toDouble()
+        val weight = rs.getString(3).toDouble()
+        val height = rs.getString(4).toDouble()
+        val gender = rs.getString(5)
+        
+        //berekening BMR mannen 
+        val BMR:Double
+        if(gender == "male")
+        {
+            BMR = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        }
+        
+        //berekening BMR mannen 
+        if(gender == "female")
+        {
+            BMR = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+        }
+        
+        //   //TDEE TOATAL DAILY ENERGY EXPENDITURE (AANTAL KCAL DIE JE LICHAAM DAGELIJKS VERBRAND ZONDER ENIGE VORM VAN INZET)
+         */
 
         return  v;
     }
